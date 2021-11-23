83d1081e8f4d2ce3e5482521ff8a0278a2ac0ff6
GuiLeoni14 <guileone167@gmail.com>
2021-11-22 23:23:45 -0200

    ajustando js

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const numeroInicial = document.getElementById("number-primary");
+const progresso = document.querySelector(".progresso");
+
+setInterval(() => {
+    if ((Number(numeroInicial.textContent)) <= 2800){
+        numeroInicial.textContent =  ((Number(numeroInicial.textContent)) + 52.7).toFixed(1);
+        if ((Number(numeroInicial.textContent)) >= 1000){
+            progresso.classList.add('meio')
+        }
+        if((Number(numeroInicial.textContent)) >= 2000){
+            progresso.classList.add('fim')
+        }
+    }    
+    // if (Number(count.textContent) == 3){
+    //     alert('limite de tempo atingido!');
+    // }
+}, 1000)
+
+
+// function getRandomIntInclusive(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max + min + 1)) + min;
+// }
