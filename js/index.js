25a64225a3c494ab1dceaa0296c70e20815dadfd
GuiLeoni14 <guileone167@gmail.com>
2021-11-23 12:08:31 -0200

    ajustando funcionalidades js

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ const progresso = document.querySelector(".progresso");
 const textoCentral = document.getElementById("texto-central");
 const download = document.getElementById('download');
 const btnInstalar = document.getElementById('instalar');
+let c = 51
+const tempo = document.getElementById('tempo');
 
 btnInstalar.addEventListener('click', (event) =>{
     event.preventDefault();
@@ -13,6 +15,8 @@ setInterval(() => {
     let classDownload = download.getAttribute('class');
     if (((Number(numeroInicial.textContent)) <= 2800) && (classDownload == 'download')){
         numeroInicial.textContent =  ((Number(numeroInicial.textContent)) + 52.7).toFixed(1);
+        c = c - 1
+        tempo.textContent = `${c}:${getRandomIntInclusive(10,60)}`
         if ((Number(numeroInicial.textContent)) >= 1000){
             progresso.classList.add('meio')
         }
@@ -21,17 +25,14 @@ setInterval(() => {
         }
         if ((Number(numeroInicial.textContent)) >= 2800){
             textoCentral.classList.add('active');
-            // download.classList.add('active');
+            tempo.textContent = `${c}:00`
         }
-    }    
-    // if (Number(count.textContent) == 3){
-    //     alert('limite de tempo atingido!');
-    // }
+    }
 }, 1000)
 
 
-// function getRandomIntInclusive(min, max) {
-//     min = Math.ceil(min);
-//     max = Math.floor(max);
-//     return Math.floor(Math.random() * (max + min + 1)) + min;
-// }
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max + min + 1)) + min;
+}
